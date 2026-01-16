1bd7752ce73e0fbc32b114746780faca65e3ba2e
Luke <ablueshark26@gmail.com>
2026-01-15 19:37:03 -0600

    commiting winter changes for muscle music gui app

--- Muscle-Music-GUI.py
+++ Muscle-Music-GUI.py
@@ -1,75 +1,125 @@
 import tkinter
 from tkinter import PhotoImage
 from PIL import ImageTk, Image
-import customtkinter
+import customtkinter as ctk
 import random
-chord = ""
 
-def getNote ():
-    global chord
+#generate random wheel and selects a chord from the wheel putting it in the highlighted tag
+def getChord ():
+    wheelLabel.configure(state = "normal")
+    wheelLabel.delete("1.0", "end")
+
     wheels = ["1", "2", "3", "4", "5"]
-    wheels =  wheels[random.randint(0, len(wheels)-1)]
-    wheelLabel.configure(text = "Wheel: " + wheels)
-    if wheels == "1":
-        w1 = ["B","C#m", "D#m", "E", "F#", "G#m"]
+    wheel =  wheels[random.randint(0, len(wheels)-1)]
+    wheels = " ".join(wheels)
+    wheelLabel .insert("end", wheels)
+    index = wheels.find(wheel)
+    start_wheel = f"1.{index}"
+    end_wheel = f"1.{index+ 1}"
+    wheelLabel.tag_add("highlight", start_wheel, end_wheel)
+
+    chordLabel.configure(state = "normal")
+    chordLabel.delete("1.0", "end")
+
+    if wheel == "1":
+        w1 = ["B", "C#m", "D#m", "E", "F#", "G#m"]
         chord =  w1[random.randint(0, len(w1)-1)]
+        w1 = " ".join(w1)
+        chordLabel.insert("end", w1)
+        index = w1.find(chord)
+        start_chord = f"1.{index}"
+        end_chord = f"1.{index+len(chord)}"
+        chordLabel.tag_add("highlight", start_chord, end_chord)
 
-    elif wheels == "2":
-        w2 = ["C", "Dm", "Em", "F", "G", "Am"]
+    elif wheel == "2":
+        w2 = ["C", "DDm", "Em", "F", "G", "Am"]
         chord =  w2[random.randint(0, len(w2)-1)]
+        w2 = " ".join(w2)
+        chordLabel.insert("end", w2)
+        index = w2.find(chord)
+        start_chord = f"1.{index}"
+        end_chord = f"1.{index+len(chord)}"
+        chordLabel.tag_add("highlight", start_chord, end_chord)
 
-    elif wheels == "3":
+    elif wheel  == "3":
         w3 = ["D♭", "E♭m", "Fm", "G♭", "A♭", "B♭m"]   
         chord =  w3[random.randint(0, len(w3)-1)]
+        w3 = " ".join(w3)
+        chordLabel.insert("end", w3)
+        index = w3.find(chord)
+        start_chord = f"1.{index}"
+        end_chord = f"1.{index+len(chord)}"
+        chordLabel.tag_add("highlight", start_chord, end_chord)
 
-    elif wheels == "4":
+    elif wheel == "4":
         w4 = ["D", "Em", "F#m", "G", "A", "Bm"]
         chord =  w4[random.randint(0, len(w4)-1)]
+        w4 = " ".join(w4)
+        chordLabel.insert("end", w4)
+        index = w4.find(chord)
+        start_chord = f"1.{index}"
+        end_chord = f"1.{index+len(chord)}"
+        chordLabel.tag_add("highlight", start_chord, end_chord)
+
     else:
         w5 = ["E♭", "Fm", "Gm", "A♭", "B♭", "Cm"]
         chord =  w5[random.randint(0, len(w5)-1)]
-    chordLabel.configure(text = chord)
+        w5 = " ".join(w5)
+        chordLabel.insert("end", w5)
+        index = w5.find(chord)
+        start_chord = f"1.{index}"
+        end_chord = f"1.{index+len(chord)}"
+        chordLabel.tag_add("highlight", start_chord, end_chord)
 
-
+    wheelLabel.tag_add("highlight", start_wheel, end_wheel)
+    chordLabel.configure(state = "disabled")
+    wheelLabel.configure(state = "disabled")
+  
 # Set the default theme and color 
-customtkinter.set_appearance_mode("Dark")
-customtkinter.set_default_color_theme("blue")
-
-3
+ctk.set_appearance_mode("Dark")
+ctk.set_default_color_theme("blue")
 
 #Creates window and font
-app = customtkinter.CTk()
+app = ctk.CTk()
 app.geometry("1100x710")
 app.title("Muscle Music")
-font1 = customtkinter.CTkFont(family="Times New Roman", size=20)
-font2 = customtkinter.CTkFont(family="Times New Roman", size=35)
-
-# Adding Title label 
-title = customtkinter.CTkLabel(app, text="Muscle Music", font=font1)
-title.pack(pady=10, padx=10)
+font_default= ctk.CTkFont(family="Times New Roman", size=35)
+font_bold= ctk.CTkFont(family="Times New Roman", size=35, weight="bold")
 
 # Adding Sound Button label 
-sound = customtkinter.CTkButton (app, text="Play Sound", command = getNote)
+sound = ctk.CTkButton (app, text="Play Sound", command = getChord)
 sound.pack(pady=10, padx=10)
 
+#Guitar frame
+guitarFrame = ctk.CTkFrame(app, width=100, height=100,fg_color="transparent")
+guitarFrame.pack(side = ctk.LEFT,pady=10, padx=10)
 
 # add the photo
 guitar_path = "guitar.png"
 guitar = Image.open(guitar_path)
-tkguitar = customtkinter.CTkImage(guitar, size = (500, 500))
-guitarImage = customtkinter.CTkLabel(app, image=tkguitar, text="")
-guitarImage.pack(side = customtkinter.LEFT, pady=10, padx=100)
-
-
-
-# Adding Wheel label 
-wheelLabel = customtkinter.CTkLabel(app, text="Wheel: none", font=font2)
-wheelLabel.pack(side = customtkinter.LEFT, pady= 10, padx=10)
+tkguitar = ctk.CTkImage(guitar, size = (500, 500))
+guitarImage = ctk.CTkLabel(guitarFrame, image=tkguitar, text="")
+guitarImage.pack( pady=10, padx=100)
 
 # Adding Note label 
-chordLabel= customtkinter.CTkLabel(app, text="none", font=font2)
-chordLabel.pack( side = customtkinter.LEFT, pady= 10, padx=50)
+chordLabel= ctk.CTkTextbox(guitarFrame, width=400, height=50, font=font_default, fg_color="transparent")
+chordLabel.pack(pady=10, padx=10)
+chordLabel.tag_config("highlight", foreground="yellow")
+chordLabel.configure(state = "disabled")
 
+# Adding Wheel frame
+wheelFrame = ctk.CTkFrame(app, width=100, height=100, fg_color="transparent")
+wheelFrame.pack(side = ctk.LEFT, pady=10, padx=10)
+
+# Adding Weel Title
+wheelTitle = ctk.CTkLabel(wheelFrame, text="Wheel", font=font_bold)
+wheelTitle.pack(pady=10, padx=(10,200))
+
+# Adding Wheel label 
+wheelLabel = ctk.CTkTextbox(wheelFrame, width=300, height=50, font=font_default, fg_color="transparent")
+wheelLabel.pack(pady= (0,200))
+wheelLabel.tag_config("highlight", foreground="yellow")
+wheelLabel.configure(state = "disabled")
 
 
 app.mainloop()
